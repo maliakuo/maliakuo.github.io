c84ec75877ca1cb45e34da07e582f0cd1a5d377e
maliakuo <70962881+maliakuo@users.noreply.github.com>
2024-04-23 23:25:09 -0400

    updates

--- interactive2024/meetcute/ohhey/ohhey.js
+++
@@ -1,6 +0,0 @@
-function revealDiv() {
-    document.getElementById("tbr").style.display = "block";
-    document.getElementById("exploring_button").style.backgroundColor = rgb(138, 0, 44);
-    document.getElementById("exploring_button").style.color = rgb(255, 240, 221);
-    document.getElementById("exploring_button").style.cursor = auto;
-}
